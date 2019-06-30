58a797e8dd09eee83049ba4dd8dca1cd1481e7b9
Ashley <28584443+tank-wsp@users.noreply.github.com>
2019-06-30 17:25:51 -0400

    Add files via upload

---
+++ vendindMachine/Soda.java
@@ -0,0 +1,20 @@
+
+public class Soda {
+
+	private String brand=" ";
+	private int price= 0;
+	/** Constructs a soda object*/
+	Soda(){
+		
+	}
+	
+	/** Constructs a soda object*/
+	Soda(String newbrand, int newprice){
+		brand=newbrand;
+		price=newprice;
+		
+	}
+
+	}
+
+
---
+++ vendindMachine/SodaMachine.java
@@ -0,0 +1,25 @@
+
+public class SodaMachine {
+private String brand;
+private int money;
+private String message;
+private Slot[][]sodaArray;
+
+/** Constructs the SodaMachine object*/
+SodaMachine(){
+	
+}
+
+/**Constructs the SodaMachine object*/
+SodaMachine(String brand, int money, String message,Slot sodaArray){
+
+}
+
+
+}
+
+
+
+	
+
+
